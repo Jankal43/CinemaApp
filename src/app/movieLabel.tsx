f908a48b803d3ae281b843e0fe699d17d2988fe8
Jan Kaletka <jankaletka43@gmail.com>
2024-12-28 22:27:19 +0100

    vercel changes

--- src/app/movieLabel.tsx
+++ src/app/movieLabel.tsx
@@ -1,14 +1,23 @@
+import Image from 'next/image';
+
 interface MovieLabelProps {
     movieTitle: string;
     moviePoster: string;
     className?: string;
 }
 
-export default function MovieLabel({movieTitle, moviePoster, className}: MovieLabelProps) {
+export default function MovieLabel({ movieTitle, moviePoster, className }: MovieLabelProps) {
     return (
         <div className={`flex flex-col w-40 h-auto flex-shrink-0 rounded-lg bg-gray-800 shadow-lg ${className}`}>
-            <div className="h-[85%]">
-                <img className="w-full h-full object-cover" src={moviePoster} alt={`${movieTitle} Poster`}/>
+            <div className="h-[85%] relative">
+                <Image
+                    className="w-full h-full object-cover rounded-t-lg"
+                    src={moviePoster}
+                    alt={`${movieTitle} Poster`}
+                    layout="fill"
+                    objectFit="cover"
+                    priority
+                />
             </div>
             <div>
                 <h3 className="text-center m-1 text-lg font-semibold truncate">{movieTitle}</h3>
