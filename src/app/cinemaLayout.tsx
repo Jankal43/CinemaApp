72f1c911289c41c7f5f501aa5db61716f96f31af
Jan Kaletka <jankaletka43@gmail.com>
2025-03-18 23:38:08 +0100

    typescript errors fixed

--- src/app/cinemaLayout.tsx
+++ src/app/cinemaLayout.tsx
@@ -1,7 +1,7 @@
 "use client"
 //dodalem ale nie wiem czy potrzeba
 import SeatMap from "@/app/seatMap";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 import ThreeScene from "@/app/ThreeScene";
 
 export default function CinemaLayout() {
